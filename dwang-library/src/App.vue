b1423665e1aa771b46dcb1c3552392b1069f24f7
randlyoyo <wangdingao5337@gmail.com>
2025-08-28 13:57:34 +1000

    update Forms and User Interaction part

--- dwang-library/src/App.vue
+++ dwang-library/src/App.vue
@@ -3,9 +3,18 @@ import HelloWorld from './components/HelloWorld.vue'
 import TheWelcome from './components/TheWelcome.vue'
 import MyName from './components/MyName.vue'
 import JSON from './components/JSON.vue'
+import Form from './components/Form.vue'
 </script>
 
+
+
 <template>
+  <!-- <JSON /> -->
+  <!-- <JSONSolution / -->
+  <Form />
+
+
+
   <header>
     <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
 
